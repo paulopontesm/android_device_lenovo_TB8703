72b587544c333825717d2363384f115cdeff77cf
Paulo Martins <paulo.pontes.m@gmail.com>
2022-07-16 00:13:51 +0100

    fix imports

--- init/Android.mk
+++ init/Android.mk
@@ -5,8 +5,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
     system/core/base/include \
-    system/core/init
-LOCAL_CPPFLAGS := -Wall
+    system/core/init \
+    external/fmtlib/include
+LOCAL_CPPFLAGS := \
+    -Wall \
+    -std=c++17
 LOCAL_SRC_FILES := init_tb8703.cpp
 LOCAL_MODULE := libinit_tb8703
 
